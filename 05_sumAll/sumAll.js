feb1d3f37039d78ca16a3e0d1d22ab21b14d6ccc
anythingzen <chengzhanxd@gmail.com>
2024-05-14 23:34:24 +0800

    Made function for summation based on start and end number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if (start < 0 || typeof(end) != 'number') {
+        return 'ERROR';
+    }
+
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+    for (let i = start; i >= end; i--) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
