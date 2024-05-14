7ab6d5ee50eaa8dd6203fd4ed86110c2574fde3f
anythingzen <chengzhanxd@gmail.com>
2024-05-14 17:37:09 +0800

    Made function for reversing a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word_to_reverse) {
+    reversed_word = '';
+    
+    // using max index, traverse backwards on the word_to_reverse and append it into reversed word.
+    for (let i = word_to_reverse.length - 1; i >= 0; i--) {
+        reversed_word += word_to_reverse[i];
+    }
 
+    return reversed_word;
 };
 
 // Do not edit below this line
