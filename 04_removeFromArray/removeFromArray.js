2a50a4294fc275613c4463f5858aeecc56f90c82
anythingzen <chengzhanxd@gmail.com>
2024-05-14 18:14:31 +0800

    Made function to remove any elements from array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(anArrayToRemove, ...elementsToRemove) {
 
+    // for each element in array to remove, return the element if it does not include elements to remove.
+    return anArrayToRemove.filter(element => !elementsToRemove.includes(element))
 };
 
 // Do not edit below this line
