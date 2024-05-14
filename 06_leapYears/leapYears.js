d4c2c4688b4fc31231f2660a554c6f236f2b6d75
anythingzen <chengzhanxd@gmail.com>
2024-05-14 23:42:38 +0800

    Made a function for calculating leap years, based on its conditions.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    
+    // divisible by 4 and 400, divisible by 100 not leap year. 
+    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+        return true;
+    }
+    return false
 };
 
 // Do not edit below this line
