ebfe86ed134866b5cf27931a0941a9b19cf8a7c8
anythingzen <chengzhanxd@gmail.com>
2024-05-14 17:16:39 +0800

    Made a function to repeat a word based on number of times the user requires.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(words, times) {
 
+    if (times <0) {
+        return 'ERROR';
+    }
+    
+    let i = 0;
+    let combined_word = '';
+    
+    // Adds in the string based on number of times user input to combine the words.
+    while (i < times) {
+        combined_word += words;
+        i++;
+    }
+    return combined_word;
 };
 
 // Do not edit below this line
