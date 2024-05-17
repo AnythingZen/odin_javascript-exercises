2494477f5bd7b5f9a16f2b7ad9be1b904b72af90
anythingzen <chengzhanxd@gmail.com>
2024-05-18 01:54:43 +0800

    completed 08 and 09.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...nums) {
+  let sum = 0;
+	nums.forEach(num => sum += num);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sumValue = 0;
+	nums.forEach(num => sumValue += num);
+  return sumValue;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let product = 1;
+	nums.forEach(num => product *= num);
+  return product;
 
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+
+	for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    // return char with alphabets only, 
+    words = words
+    .split('')
+    .filter(word => /[a-zA-Z0-9]/.test(word))
+    .join('')
+    .toLowerCase(); 
+    
+    let index = words.length;
 
+    for (let i = 0; i < index; i++) {
+
+        if (index - 1 == i) {
+            return true;
+        }
+
+        else if (words[i] !== words[index - (1 + i)]) {
+            return false;
+        }
+    }
 };
 
 // Do not edit below this line
