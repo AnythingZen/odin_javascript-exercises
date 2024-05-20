42f670a76e5f36954bda695f2b951b470e6261db
anythingzen <chengzhanxd@gmail.com>
2024-05-20 23:34:08 +0800

    Finished 10 - 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+function getAge (birth, dead) {
+    if (!dead) {
+        dead = new Date().getFullYear();
+    }
 
+    return dead - birth;
+}
+
+const findTheOldest = function(people) {
+    const oldest = people.reduce((ageTotal, ageCurrent) => {
+                    const person1 = getAge(ageTotal.yearOfBirth, 
+                                        ageTotal.yearOfDeath);
+                    const person2 = getAge(ageCurrent.yearOfBirth, 
+                                        ageCurrent.yearOfDeath);
+
+                    return person1 < person2 ? ageCurrent : ageTotal;
+                });
+    return oldest;
 };
 
 // Do not edit below this line
